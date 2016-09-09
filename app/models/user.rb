b2619a7aebb3fea2a0293e135064177bcb1cbce5
oyama919 <oyama919@gmail.com>
2016-09-09 17:52:22 +0900

    tst

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:confirmable
-  has_many :blogs, :contacts
+  has_many :blogs
+  has_many :contacts
 end
