0169c677396ab7bf3ed7c9c93f35b1d2b4ce9058
oyama919 <oyama919@gmail.com>
2016-09-09 19:23:04 +0900

    contact

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -11,10 +11,5 @@ class NoticeMailer < ApplicationMailer
     mail to: "oyama919@gmail.com",
          subject: '【Achieve】ブログが投稿されました'
   end
-  def sendmail_contact(contact)
-    @contact = blog
-
-    mail to: "oyama919@gmail.com",
-         subject: '【Achieve】お問合せが送信されました'
-  end
+  
 end
