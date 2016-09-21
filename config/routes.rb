ef197a5239e9ae21425a712970b78e407df0fb44
oyama919 <oyama919@gmail.com>
2016-09-21 19:47:10 +0900

    db naosita

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users
    resources :blogs do #, only: [:index, :new, :create, :edit, :update, :destroy] do
      collection do
