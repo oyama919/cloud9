38714dc2f9a9453abb01a06f3be105e998b6f26c
oyama919 <oyama919@gmail.com>
2016-12-04 16:37:29 +0900

    add destroybutton

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,9 @@ class User < ActiveRecord::Base
   def follow!(other_user)
     relationships.create!(followed_id: other_user.id)
   end
+  def unfollow!(other_user)
+  relationships.find_by(followed_id: other_user.id).destroy
+  end
   #フォローしているかどうかを確認する
   def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
