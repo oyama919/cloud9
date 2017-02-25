aa22aebe186040952fa1d17342c589db79825fe8
oyama919 <oyama919@gmail.com>
2017-02-25 17:22:06 +0900

    DIVE19_1_メッセージ機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,30 +9,33 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
-    }
+  }
 
-    resources :blogs do #, only: [:index, :new, :create, :edit, :update, :destroy] do
-      resources :comments
-      collection do
-        post :confirm
-      end
+  resources :blogs do #, only: [:index, :new, :create, :edit, :update, :destroy] do
+    resources :comments
+    collection do
+      post :confirm
     end
+  end
 
-
-    resources :contacts, only: [:index, :new, :create] do
-      collection do
-        post :confirm
-      end
+  resources :contacts, only: [:index, :new, :create] do
+    collection do
+      post :confirm
     end
+  end
 
-    resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show]
 
-    resources :relationships, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
-   root 'top#index'
+  resources :conversations do
+    resources :messages
+  end
 
-   if Rails.env.development?
-     mount LetterOpenerWeb::Engine, at: "/letter_opener"
-   end
+  root 'top#index'
+
+  if Rails.env.development?
+   mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 
 end
