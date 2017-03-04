981165cb125b3ecdf47a084c4b4bc526e9af48ee
oyama919 <oyama919@gmail.com>
2017-03-04 15:42:44 +0900

    production.rb domain 変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   {
    user_name: ENV['SENDGRID_USERNAME'],
    password: ENV['SENDGRID_PASSWORD'],
-   domain: "heroku.com",
+   domain: "",
    address: "smtp.sendgrid.net",
    port: 587,
    authentication: :plain,
