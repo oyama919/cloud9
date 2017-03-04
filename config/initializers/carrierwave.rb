26a71894ae8d3e6c4c88aacf9195ee84646b2baa
oyama919 <oyama919@gmail.com>
2017-03-04 15:35:30 +0900

    production.rb mailerホスト変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,13 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.fog_directory = 'cloud9-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cloud9-production'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
-    when 'test'
-      config.fog_directory = 'テスト環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/テスト環境用のバケット名'
+      config.fog_directory = 'cloud9-development'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cloud9-development'
   end
 end
